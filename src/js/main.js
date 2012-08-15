4ed3f596f61a34a602c9ff81499927d357327c48
Phil Leggetter <phil@leggetter.co.uk>
2012-08-15 13:02:29 +0100

    first commit of *very* basic Pusher example

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+forge.logging.info("Add JavaScript to js/main.js!");
+
+Pusher.log = forge.logging.info;
+
+var pusher = new Pusher( '49e26cb8e9dde3dfc009' );
+
+forge.logging.info("Created Pusher instance");
