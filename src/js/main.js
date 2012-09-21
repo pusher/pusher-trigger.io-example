acec7f63e33feac7bd3d168e65659648d999d4e5
Phil Leggetter <phil@leggetter.co.uk>
2012-09-21 13:22:54 +0100

    making it actually useful

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,35 @@
-forge.logging.info("Add JavaScript to js/main.js!");
+forge.logging.info("Loaded!");
 
 Pusher.log = forge.logging.info;
 
-var pusher = new Pusher( '49e26cb8e9dde3dfc009' );
-
-forge.logging.info("Created Pusher instance");
+( function( $ ) {
+	
+  var CONFIG = {
+			PUSHER: {
+		    APP_KEY: '49e26cb8e9dde3dfc009'
+			}
+	};
+	
+  // Connect
+	var pusher = new Pusher(CONFIG.PUSHER.APP_KEY);
+	pusher.connection.bind('state_change', connectionStateChange);
+	
+	function connectionStateChange(state) {
+		$('#connectionStatus').html(state.current);
+	}
+	
+	// Subscribe
+	var channel = pusher.subscribe('my-channel');
+	channel.bind('pusher:subscription_succeeded', subscriptionSucceeded);
+	
+	function subscriptionSucceeded() {
+		$('#subscriptionStatus').html('succeeded');
+	}
+	
+	channel.bind('my-event', handleMyEvent);
+	
+	function handleMyEvent( data ) {
+		$('#myEventData').append('<pre>' + JSON.stringify(data, null, 2) + '</pre>');
+	}
+  
+} )( jQuery );
